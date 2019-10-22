c79ab439f22229d6e764c879d81e2713dd27232b
Blake Sin <sinblake@gmail.com>
2019-10-22 09:17:21 -0700

    a

--- PalindromeTester.java
+++ PalindromeTester.java
@@ -5,13 +5,33 @@ public class PalindromeTester{
     Scanner scan = new Scanner(System.in);
     System.out.println("would you like to enter a palindrome? (y/n)");
     String answer = scan.nextLine();
-    if(answer == "y"){
-      System.out.println("Enter a palindrome");
+    String N = "n";
+    String reversePalindrome = "";
+    String letter = "";
+    answer = answer.toLowerCase();
+    while(answer.compareTo(N) != 0){
+      System.out.println("Enter a palindrome enter 0 to exit");
+      String O = "0";
+      reversePalindrome = "";
       String palindrome = scan.nextLine();
+      if(palindrome.compareTo(O)==0){
+        break;
+      }
+      palindrome = palindrome.replaceAll("\\s+", "");
+      palindrome = palindrome.replaceAll(",", "");
+      palindrome = palindrome.replaceAll(".", "");
+      palindrome = palindrome.toLowerCase();
       int palindromeLength = palindrome.length();
-      System.out.println(palindromeLength);
-    }else{
-      System.out.println("Bye");
+      for(int i=palindromeLength; i>0; i--){
+        letter = palindrome.substring(i-1,i);
+        reversePalindrome = reversePalindrome + letter;
+      }
+      if(palindrome.compareTo(reversePalindrome)==0){
+        System.out.println("this is a palindrome");
+      }else{
+        System.out.println("this isn't a palindrome");
+      }
     }
+    System.out.println("Bye");
   }
 }
--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -5,17 +5,95 @@ public class RockPaperScissors{
   public static void main(String[] args){
     Random rand = new Random();
     Scanner scan = new Scanner(System.in);
-    String userInput = scan.nextLine();
-    String O = "0";
-    System.out.println("choose Rock(R), Paper(P), or Scissors(S). enter 0 to escape");
-    userInput = userInput.toUpperCase();
-    while(userInput != O){
-    int computerInput = rand.nextInt(3)+1;
-    System.out.println(userInput);
-    System.out.println(computerInput);
-    if(userInput.equals("R"))
-    System.out.println("enter R, P, or S to play again. enter 0 to escape");
-    userInput = scan.nextLine();
+
+    int wins = 0;
+    int games = 0;
+    //Explaining the inputs
+    System.out.println("choose Rock(1), Paper(2), or Scissors(3). enter 0 to escape");
+    int userInput = scan.nextInt();
+
+    //Determine if the first input is valid
+    while(userInput>3 || userInput<0){
+      System.out.println("must choose a number between 1 and 3");
+      userInput = scan.nextInt();
     }
+
+    while(userInput != 0){
+    int computerInput = rand.nextInt(3)+1;
+    String userChoice = "";
+    String computerChoice = "";
+
+    //Make it display yours and the computers choices instead of numbers
+    if(userInput == 1){
+      userChoice = "Rock";
+    }
+    if(userInput == 2){
+      userChoice = "Paper";
+    }
+    if(userInput == 3){
+     userChoice = "Scissors";
+    }
+    if(computerInput == 1){
+      computerChoice = "Rock";
+    }
+    if(computerInput == 2){
+      computerChoice = "Paper";
+    }
+    if(computerInput == 3){
+      computerChoice = "Scissors";
+    }
+    System.out.println("you chose "+userChoice);
+    System.out.println("the computer chose "+computerChoice);
+    //Deciding who wins Rock(1) Paper(2) Scissors(3)
+      if((computerInput == 1) && (userInput == 3)){
+        System.out.println("You Lose");
+        games++;
+      }
+      if((computerInput == 1) && (userInput == 2)){
+        System.out.println("You Won");
+        wins++;
+        games++;
+      }
+      if((computerInput == 1) && (userInput == 1)){
+        System.out.println("You Tied");
+        games++;
+      }
+      if((computerInput == 2) && (userInput == 3)){
+        System.out.println("You Won");
+        wins++;
+        games++;
+      }
+      if((computerInput == 2) && (userInput == 2)){
+        System.out.println("You Tied");
+        games++;
+      }
+      if((computerInput == 2) && (userInput == 1)){
+        System.out.println("You Lose");
+        games++;
+      }
+      if((computerInput == 3) && (userInput == 3)){
+        System.out.println("You Tied");
+        games++;
+      }
+      if((computerInput == 3) && (userInput == 2)){
+        System.out.println("You Lose");
+        games++;
+      }
+      if((computerInput == 3) && (userInput == 1)){
+        System.out.println("You Won");
+        wins++;
+        games++;
+      }
+    //Checks again if the input is valid
+    System.out.println("enter Rock(1), Paper(2), or Scissors(3) to play again. enter 0 to escape");
+    userInput = scan.nextInt();
+    while(userInput>3 || userInput<0){
+      System.out.println("must choose a number between 1 and 3");
+      userInput = scan.nextInt();
+    }
+    }
+    double winPercentage = ((double) wins/games)*100;
+
+  System.out.println("your win percentage is "+ Math.round(winPercentage)+"%");
   }
 }
